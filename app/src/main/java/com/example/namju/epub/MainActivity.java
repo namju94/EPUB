244bb68644465edb3b9324f515efb6a9e028a692
namju94 <namjukim94@gmail.com>
2017-02-09 17:38:07 -0800

    Initial commit

---
+++ app/src/main/java/com/example/namju/epub/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.namju.epub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.TOCReference;
+import nl.siegmann.epublib.epub.EpubReader;
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AssetManager assetManager = getAssets();
+        try {
+            // find InputStream for book
+            InputStream epubInputStream = assetManager.open("books/testbook.epub");
+
+            // Load Book from inputStream
+            Book book = (new EpubReader()).readEpub(epubInputStream);
+            // Log the book's authors
+            Log.i("epublib", "author(s): " + book.getMetadata().getAuthors());
+            // Log the book's title
+            Log.i("epublib", "title: " + book.getTitle());
+            // Log the book's coverimage property
+            Bitmap coverImage = BitmapFactory.decodeStream(book.getCoverImage().getInputStream());
+            Log.i("epublib", "Coverimage is " + coverImage.getWidth() + " by " + coverImage.getHeight() + " pixels");
+            // Log the tale of contents
+            logTableOfContents(book.getTableOfContents().getTocReferences(), 0);
+        } catch (IOException e) {
+            Log.e("epublib", e.getMessage());
+        }
+    }
+
+    /**
+     * Recursively Log the Table of Contents
+     *
+     * @param tocReferences
+     * @param depth
+     */
+    private void logTableOfContents(List<TOCReference> tocReferences, int depth) {
+        if (tocReferences == null) {
+            return;
+        }
+        for (TOCReference tocReference : tocReferences) {
+            StringBuilder tocString = new StringBuilder();
+            for (int i = 0; i < depth; i++) {
+                tocString.append("\t");
+            }
+            tocString.append(tocReference.getTitle());
+            Log.i("epublib", tocString.toString());
+
+            logTableOfContents(tocReference.getChildren(), depth + 1);
+        }
+    }
+}
